6e21cee2d7d43c93b7ba52cedcc1845ac98e68ce
kenchan0130 <tt.tanishi100@gmail.com>
2023-10-06 09:37:33 +0900

    fix

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import puppeteer from 'puppeteer';
 import axios from 'axios';
-import { JSDOM } from "jsdom";
+import { JSDOM } from 'jsdom';
+import { performance } from 'perf_hooks';
 
 const sleep = ms => new Promise(res => setTimeout(res, ms))
 const loopWaitMs = 5000; // 5秒
-const loopLimitMs = 1000 * 60 * 10; // 10分
+const loopLimitMs = 1000 * 60 * 30; // 30分
 
 const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
 const url = `https://reserve.tokyodisneyresort.jp/sp/restaurant/list/?useDate=20231010&mealDivInform=&adultNum=4&childNum=2&childAgeInform=01%7C02%7C&restaurantTypeInform=4&restaurantNameCd=&wheelchairCount=0&stretcherCount=0&showWay=&reservationStatus=1&beforeUrl=https%3A%2F%2Freserve.tokyodisneyresort.jp%2Fsp%2Frestaurant%2Flist%2F%3FuseDate%3D20231030%26mealDivInform%3D%26adultNum%3D4%26childNum%3D2%26childAgeInform%3D01%257C02%257C%26restaurantTypeInform%3D4%257C1%26restaurantNameCd%3D%26wheelchairCount%3D0%26stretcherCount%3D0%26showWay%3D%26reservationStatus%3D1%26wayBack%3D&wayBack=`;
@@ -13,14 +14,21 @@ if (!slackWebhookUrl) {
   throw "Please set SLACK_WEBHOOK_URL"
 }
 
+
 (async () => {
-  let counter = 1;
-  let counterMs = 0;
+  let counter = 0;
+  const start = performance.now();
 
   console.log(`Loading ${url} ...`);
   while (true) {
-    if (counterMs > loopLimitMs) {
+    counter++
+    const lap = performance.now();
+    if ((lap - start) > loopLimitMs) {
       break
+    }
+
+    if (counter > 1) {
+      await sleep(loopWaitMs);
     }
 
     const browser = await puppeteer.launch({
@@ -44,14 +52,12 @@ if (!slackWebhookUrl) {
     if (hasGotReservationDom.length == 0) {
       console.log("No found seats");
       await browser.close();
-      await sleep(loopWaitMs);
-      counterMs += loopWaitMs
-      counter++
       continue
     }
 
     const results = Array.from(hasGotReservationDom).map((v) => v.querySelector(".name").textContent);
 
+    console.log("Found seats!!")
     await axios.post(
       slackWebhookUrl,
       JSON.stringify({
@@ -60,9 +66,6 @@ if (!slackWebhookUrl) {
     )
 
     await browser.close();
-    await sleep(loopWaitMs);
-    counterMs += loopWaitMs
-    counter++
   }
 
 })();
